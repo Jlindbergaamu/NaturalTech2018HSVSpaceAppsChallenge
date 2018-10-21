0195515194c1af82b125ff5cac2115a334ad70c9
Jlindbergaamu <31498019+Jlindbergaamu@users.noreply.github.com>
2018-10-21 14:10:00 -0500

    theDatabase

---
+++ naturalTech.swift
@@ -0,0 +1,67 @@
+﻿var mossIdentityString = "Moss!\n Scientific Name : Bryophyta\n";
+ var mossFactOneString = "Bryophytes are the closest living relatives to the very first plants to make land on the planet, approximately 500 million years ago.\n";
+ var mossFactTwoString = "Moss is a good bioindicator of air pollution.\n";
+ var mossFactThreeString = "Sphagnum moss also has antiseptic properties. In wounded humans for example, Sphagnum moss maintains a low ph level around the wound.\n";
+ var mossResult = mossIdentityString + mossFactOneString + mossFactTwoString + mossFactThreeString;
+ //print(mossResult); This display statement was only used to test if the previous variables were correct. May need to delete depending if the loop and booleans below work.
+ var mossImage = true; /*this is the conditional, which when put into Xcode will be changed into a button, so if the button is clicked, that makes mossImage true
+ which then tells me facts about moss. Or when the user uploads their photo, this information will then be displayed. */
+ if(mossImage == true)
+ {
+ print(mossResult);
+ }
+ var wreathIdentityString = "Alabama Snow-Wreath!\n Scientific Name : Neviusia Alabamensis.\n ";
+ var wreathFactOneString = "This plant grows easiest in average, medium moisture.\n";
+ var wreathFactTwoString = "Is a deciduous shrub that grows to 3-6’ tall and as wide with erect stems.\n";
+ var wreathFactThreeString = "These flowers give way to somewhat inconspicuous fruits (drupe-like achenes) that mature in early fall.\n";
+ var wreathResult = wreathIdentityString + wreathFactOneString + wreathFactTwoString + wreathFactThreeString;
+ var wreathImage = true;
+ if(wreathImage == true){
+     print(wreathResult);
+ }
+ var araliaIdentityString = "Devil's Walkingstick\n Scientific Name : Aralia Spinosa\n";
+ var araliaFactOneString = "It is an aromatic spiny deciduous shrub or small tree growing 2–8 m (6.6–26.2 ft) tall\n";
+ var araliaFactTwoString = "The young leaves can be eaten if gathered before the prickles harden\n";
+ var araliaFactThreeString = "The leaves can be finely chopped and cooked as a potherb\n";
+ var araliaResult = araliaIdentityString + araliaFactOneString + araliaFactTwoString + araliaFactThreeString;
+ var araliaImage = true;
+ if(araliaImage == true)
+ {
+     print(araliaResult);
+ }
+ var vineIdentityString = "Cross Vine!\n Scientific Name : Bignonia capreolata\n";
+ var vineFactOneString = "A vigorous, woody vine that climbs by branched tendrils with adhesive disks.\n";
+ var vineFactTwoString = "It is grown primarily for its attractive flowers and its ability to rapidly cover structures with attractive foliage.\n";
+ var vineFactThreeString = "Tendril branches terminate in adhesive disks that easily attach to walls.\n";
+ var vineResult = vineIdentityString + vineFactOneString + vineFactTwoString + vineFactThreeString;
+ var vineImage = true;
+ if(vineImage == true){
+     print(vineResult);
+ }
+ var sunflowerIdentityString = "Swamp Sunflower!\n Scientific Name : Helianthus angustifolius\n";
+ var sunflowerFactOneString = "This flower is very beneficial to native bees!\n";
+ var sunflowerFactTwoString = "Needs wet soil to grow such as a bog or pond area.\n";
+ var sunflowerFactThreeString = "Well-known for attracting birds.\n";
+ var sunflowerImage = true;
+ var sunflowerResult = sunflowerIdentityString + sunflowerFactOneString + sunflowerFactTwoString + sunflowerFactThreeString;
+ if(sunflowerImage == true){
+     print(sunflowerResult);
+ }
+ var OakIdentity = "Oak!\n"
+var OakString = "The Quercus, commonly known as Oak is a tree or shrub in the genus of the beech famiy, fagaceae.\nMedicinal Uses:\nThe bark is used to treat arthritis, diarrhea, colds, fever, cough, and bronchitis; for stimuating appetite; and or improving digestion.\nOak trees grow edible 'fruit' in the form acorns. All other parts are edible, but often contain large quantaties of bitter subsances.\nAcorns can be boiled to get remove bitter tatse, and can also be ground into four for baking.\nWARNING WARNING WARNING!!! Tannic acid gives acorns their bitter taste. Eating an excessive amount of acorns high in tannic acid can lead to kidney failure.\nSurvival:\nDue to its high density, strength, and hardness, Oak wood is excellent for building or burning. Smaller Oak can be split and cut into long thin strips.\n\n"
+    var OakImage = true
+    if OakImage == true{
+        print(OakIdentity,OakString)
+    }
+var PoisonIvyIdentity = "Poison Ivy!\n"
+var PoisonIvyString = "Toxicodendron radicans, commonly called poison ivy, is a poisonous Asian and Eastern North American flowering plant that is well known for causing urushiol-induced contact dermatitis, an itchy irritating, and sometimes painful rash.\nMedical Uses:\nAlthough harmful to humans, poison ivy is considered a beneficial native plant for wildlife. The leaves of poison ivy were once used for treating liver disorders.\nFirst Response Treatment:\nIf contact is made with skin,wash contacted area COLD water, as warm water will open up the pours of the skin allowing for the rash to appear more severely\n\n"
+    var PoisonIvyImage = true
+    if PoisonIvyImage == true{
+        print(PoisonIvyIdentity,PoisonIvyString)
+    }
+var crotonIdentity = "croton!\n";
+var crotonString = "ENDANGERED WATCH LIST!!\nThe Alabamensis Croton is a semi-evergreen woodland shrub found only in Bibb and Tuscaloosa counties of central Alabama\ncroton image is associated with medical properties, which this plant has.\nFor many centuries it was used even in Chinese medicine to heal various diseases.Use it if you have carbuncles, itching or even eczema. Some people believe that it can be helpful if one applies it to cancer lesions or various tumors.\nIf you suffer from diarrhea, nausea and even dysentery, a croton will help.Sometimes people use it to treat flu and cold. You can also use it if you suffer from toothache.\nWARNING!!!\nIt is a poisonous plant in its natural environment, but still it contains elements, which can help you to get rid of bothering health problems.\nParts of a Croton are poisonous if ingested.\n";
+    var crotonImage = true
+    if crotonImage == true{
+        print(crotonIdentity,crotonString)
+    }
